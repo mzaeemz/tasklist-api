206e72549712a3147412bee71e5ce8a48eb73e3d
M Zaeem Zahid <zaeem@datumbrain.com>
2020-06-18 22:20:29 +0500

    [TL-9] Adding Commad Line Arguments Support in run.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 TASKLIST_PORT=7850
 export TASKLIST_PORT
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,37 @@
-#!/usr/bin/env bash
+#!/bin/bash
 source ./scripts/env.sh     #access all the environment variables in env.sh
-sbt -Dhttp.port="$TASKLIST_PORT" run    #run sbt server on port 7850
+
+CLEAN="FALSE"
+usage()
+{
+    echo "Usage:"
+    echo "args.sh -h "
+    echo "args.sh -c "
+    echo "args.sh -p <port> "
+    echo ""
+    echo "   -c         to execute sbt clean before sbt run"
+    echo "   -p <port>  to run sbt with http.port=port"
+    echo "   -h         help (this output)"
+    exit 0
+}
+
+while getopts "cp:h" o; do
+    case "${o}" in
+        c)
+            CLEAN="TRUE"
+            ;;
+        p)
+            TASKLIST_PORT=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+if [[ ${CLEAN} = "TRUE" ]]; then
+    sbt clean;
+fi
+sbt -Dhttp.port="$TASKLIST_PORT" run;    #run sbt server on port 7850
